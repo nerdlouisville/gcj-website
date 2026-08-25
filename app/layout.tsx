6ec5d83cbfa20299ed3eb9370a9c7a9d1ca7707d
Bill Henderson <hendersonw2@gmail.com>
2026-08-24 20:53:05 -0400

    fix: load Meta Pixel with beforeInteractive so it renders in static HTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable}`}>
       <body className="min-h-screen flex flex-col antialiased">
-        <Script id="meta-pixel" strategy="afterInteractive">
+        <Script id="meta-pixel" strategy="beforeInteractive">
           {`!function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
